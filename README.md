6ebb7765b9a74693e9b82fe7a79742d029514c96
AdrianoCelentano <adrian.schaefer87@gmail.com>
2021-02-28 12:32:07 +0100

    apply formatting

--- README.md
+++ README.md
@@ -29,7 +29,7 @@ The animation Api is just awesome.
 ## License
 ```
 Copyright 2020 The Android Open Source Project
-
+    
 Licensed under the Apache License, Version 2.0 (the "License");
 you may not use this file except in compliance with the License.
 You may obtain a copy of the License at
--- app/src/main/java/com/example/androiddevchallenge/DogDetailsView.kt
+++ app/src/main/java/com/example/androiddevchallenge/DogDetailsView.kt
@@ -1,3 +1,18 @@
+/*
+ * Copyright 2021 The Android Open Source Project
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     https://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
 package com.example.androiddevchallenge
 
 import androidx.compose.animation.core.Animatable
@@ -6,8 +21,8 @@ import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.material.Card
 import androidx.compose.runtime.Composable
-import androidx.compose.runtime.MutableState
 import androidx.compose.runtime.LaunchedEffect
+import androidx.compose.runtime.MutableState
 import androidx.compose.runtime.remember
 import androidx.compose.runtime.rememberCoroutineScope
 import androidx.compose.ui.BiasAlignment
@@ -42,6 +57,5 @@ fun DogDetailsView(dog: MutableState<Dog?>, dogValue: Dog) {
         ) {
             DogView(dog = dogValue)
         }
-
     }
 }
